dda104d57f6748459a8f59b29fbe8fcd99a38c62
N1stDev <nist.dev@yandex.ru>
2023-06-13 23:22:42 +0300

    оставил комментарии + преран

---
+++ prerun.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    import subprocess
+
+    subprocess.Popen("pip install beautifulsoup4")
+    subprocess.Popen("pip install django")
+    subprocess.Popen("pip install requests")
